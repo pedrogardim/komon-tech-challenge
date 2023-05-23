cd02836301315ba0abf56e7169f659e4e05bd9ab
Pedro Henrique Gardim Miranda <me@pedrogardim.com>
2023-05-23 19:29:40 +0200

    Add integrations improvements and fixes

--- app/integrations/new/[integrationOption]/page.tsx
+++ app/integrations/new/[integrationOption]/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import StatsGrid from '@/components/integrations/StatsGrid';
@@ -14,11 +14,28 @@ import { SocialProfile } from '@/data/mockProfileData';
 
 const NewConnectionPage: React.FC = () => {
   const router = useRouter();
-  const { newIntegration, update } = useIntegrationsContext();
+  const { integrations, newIntegration, update } = useIntegrationsContext();
+  const [label, setLabel] = useState<String>('');
+  const [error, setError] = useState<null | String>(null);
+
   const data = newIntegration as Integration & SocialProfile;
 
-  const addConnection = () => {
-    const newIntegration = {};
+  const onAddConnection = () => {
+    if (!label.trim()) {
+      setError('Label should not be empty');
+      return;
+    }
+    const newIntegration = {
+      id: data.type + '-' + data.username,
+      username: data.username,
+      type: data.type,
+      label,
+    };
+    if (integrations.find((int) => int.id === newIntegration.id)) {
+      setError('This account is already connected');
+      return;
+    }
+    //TODO: Mock PUT Endpoint
     update((prev: IntegrationsCtxState) => ({
       integrations: [...prev.integrations, newIntegration],
       newIntegration: null,
@@ -26,10 +43,14 @@ const NewConnectionPage: React.FC = () => {
     router.replace('/integrations');
   };
 
-  const cancel = () => {
+  const onCancel = () => {
     update({ newIntegration: null });
     router.replace('/integrations');
   };
+
+  useEffect(() => {
+    setError(null);
+  }, [label]);
 
   useEffect(() => {
     if (!newIntegration) router.replace('/integrations');
@@ -38,16 +59,24 @@ const NewConnectionPage: React.FC = () => {
   if (!newIntegration) return null;
 
   return (
-    <div className="py-4 px-14">
+    <div className="p-4">
       <h1 className="text-xl my-2">Integration Label</h1>
-      <div className="border-b border-gray-300 w-1/3 mb-4 focus-within:border-gray-500">
+      <div
+        className={`relative border-b border-gray-300 w-1/3 mb-4  ${
+          error ? 'border-red-500' : 'focus-within:border-gray-500'
+        }`}
+      >
         <input
-          className="border-none p-2 px-4 text-sm border-b w-full focus:outline-0 rounded-xl"
+          className="border-none p-2 text-sm border-b w-full focus:outline-0 rounded-xl"
           placeholder="Main Instagram, My Secondary YouTube Channel..."
+          onChange={(e) => setLabel(e.target.value)}
         />
+        <span className="absolute text-xs text-red-500 -bottom-6 left-2">
+          {error}
+        </span>
       </div>
-      <h1 className="text-xl my-4 mt-6">Profile Info</h1>
-      <div className="grid grid-cols-4 gap-4 mt-4">
+      <h1 className="text-xl my-4 mt-10">Profile Info</h1>
+      <div className="grid grid-cols-4 gap-4 mt-4 p-8 border shadow">
         <div className="flex justify-center items-center">
           <Image
             className="h-48 w-48 object-cover rounded-full"
@@ -77,13 +106,13 @@ const NewConnectionPage: React.FC = () => {
       </div>
       <div className="flex justify-end mt-8">
         <button
-          onClick={cancel}
+          onClick={onCancel}
           className="text-lg py-2 px-4 rounded-xl hover:shadow duration-150 mr-2"
         >
           Cancel
         </button>
         <button
-          onClick={addConnection}
+          onClick={onAddConnection}
           className="text-lg text-white bg-black py-2 px-4 rounded-xl hover:shadow-xl duration-150"
         >
           Add connection
